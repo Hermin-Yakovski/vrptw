3ae619f75c7624d9abfc362cc47147f6cf46b4ec
yehemin <hemin.ye@qq.com>
2026-08-07 17:00:52 +0800

    feat(algorithm): switch pipeline to GreedySolver
    - Import GreedySolver in UnifiedCapacityAlgorithm
    - Replace UnifiedCapacitySolver (LP) with GreedySolver in pipeline
    - Remove stray pdb breakpoint from greedy_solver.py

--- vrptw/algorithm/_unified_capacity_algorithm.py
+++ vrptw/algorithm/_unified_capacity_algorithm.py
@@ -1,10 +1,11 @@
 from or_algo import Algorithm
 
-from ._solver import UnifiedCapacitySolver, RouteExtractor
+from ._solver import UnifiedCapacitySolver, RouteExtractor, GreedySolver
 
 
 class UnifiedCapacityAlgorithm(Algorithm):
     def __init__(self, *args, capacity: float, **kwargs):
         super().__init__(*args, **kwargs)
-        self.append(UnifiedCapacitySolver, 'UnifiedCapacitySolver', capacity=capacity)
+        # self.append(UnifiedCapacitySolver, 'UnifiedCapacitySolver', capacity=capacity)
+        self.append(GreedySolver, 'GreedySolver', capacity=capacity)
         self.append(RouteExtractor, 'RouteExtractor')
